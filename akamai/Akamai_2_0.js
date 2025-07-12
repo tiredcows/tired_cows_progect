f1d2aab38a4b2b7bbff15d8599b8fa32757964b8
tiredcows <thb1658778534@163.com>
2025-07-12 10:31:25 +0800

    Add files via upload

---
+++ akamai/Akamai_2_0.js
@@ -0,0 +1,560 @@
+window = global;
+
+Cj6 = [
+    "-100",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36,uaend,12147,20030107,en-GB,Gecko,5,0,0,0,423070,0,1707,912,1707,960,728,3165,1707,,cpen:0,i1:0,dm:0,cwen:0,non:1,opc:0,fc:0,sc:0,wrc:1,isc:0,vib:1,bat:1,x11:0,x12:1,8106,0.08343344641,859734051658,0,0,loc:",
+    "-105",
+    "0,0,0,0,4866,113,0;0,-1,0,1,4999,1101,0;0,0,0,0,4892,113,0;0,-1,0,1,4757,1101,0;0,-1,0,1,4818,1101,0;0,0,0,0,4881,113,0;0,-1,0,1,4880,1101,0;0,0,0,0,4948,113,0;",
+    "-108",
+    "",
+    "-101",
+    "do_en,dm_en,t_en",
+    "-110",
+    "",
+    "-117",
+    "",
+    "-109",
+    "0,8294,-1,-1,-1,-1,-1,-1,-1,-1,-1;",
+    "-102",
+    "0,0,0,0,4866,113,0;0,-1,0,1,4999,1101,0;0,0,0,0,4892,113,0;0,-1,0,1,4757,1101,0;0,-1,0,1,4818,1101,0;0,0,0,0,4881,113,0;0,-1,0,1,4880,1101,0;0,0,0,0,4948,113,0;",
+    "-111",
+    "0,8298,-1,-1,-1;",
+    "-114",
+    "",
+    "-103",
+    "",
+    "-106",
+    "9,1",
+    "-115",
+    "1,32,32,8298,8294,0,16592,8803,0,1719468103316,6,18394,0,0,3065,0,0,8804,16592,0,A71F92AB598F0BB486F9EF39AF78C98D~-1~YAAQR/6Yc65vlzaQAQAAXRRJWAxrKJnNTJAfqRkvBK0H33+OgOXN1PIRl0ZRQrBZ/u3PE58ArV8YqTfpsnjUiTTrS3n+Yv/JEvEOyGNabRdkLD3caXBK/T9bMBCw3399rANU+fBUgEdkAwgQAR7Ydrg5BmxkwNpH4YEsVwwhIPE8K2G9Ca4z03yGEST+8lwFiATDyYjaNgpPeaKXyMZeGJkjQfUZoKQps30seu6Q+HmzxzHODyxs/vyMRtfDquNze41JQ083iODzUTySN9So7NCUh7QtDHh82/u/Xjla/R0GG0ssnuSq2taYXZl4DzLBaPUK0faUgcpYA18YfXj7SYQBvyiUE36jTyL7TFJEz9nyW2g8XzCe6qVZqec9PuqVV1GTMdFsKtUfr8YrKIuru3K7e0/joM8Ep9SFsHTd~-1~-1~-1,38839,463,-1377006262,30261693,PiZtE,99096,36,0,0,0,,,878f03a209fc8f4453856b4b6b8a6f15d6b86233fd611e9cb553e8077248c32b,228",
+    "-112",
+    "https://www.dhl.com/cn-zh/home/tracking/tracking-ecommerce.html?submit=1&tracking-id=123123",
+    "-119",
+    "-1",
+    "-122",
+    "0,0,0,0,1,0,0",
+    "-123",
+    "",
+    "-124",
+    "",
+    "-126",
+    "",
+    "-127",
+    "11310044241322243122",
+    "-128",
+    ",,",
+    "-131",
+    ",,,",
+    "-132",
+    "",
+    "-133",
+    "",
+    "-70",
+    "-717190152;-1;dis;,7;true;true;true;-480;true;24;24;true;false;-1",
+    "-80",
+    "5052",
+    "-90",
+    "2304|16592",
+    "-116",
+    0,
+    "-129",
+    "0513f7c636866c4df96a95ce09ba4c726e1156716753a16e639d5d039b0efbb1,0.375,e6a6b62042f2333998b8357baa6fe8b776aaad3c0b1692df334f2afaf3954baf,WebKit;wl1,WebKit WebGL;wl2,481bdb3423f1a79606be78ed3f6cbc5d5dffd9ad02b0e61036ae14646bd96721,35,Google Inc. (NVIDIA);wl3,ANGLE (NVIDIA, NVIDIA GeForce RTX 3060 Laptop GPU (0x00002520) Direct3D11 vs_5_0 ps_5_0, D3D11);wl4,1f2ff65eb08d5fda390d5e11491bb7631888266547c82ad85f994b40b732c0bb,03b8256a6caaaef793e5e8b835cd4d2a73f0e4691691cc242286358ed93d1ff8",
+    "-129",
+    "0513f7c636866c4df96a95ce09ba4c726e1156716753a16e639d5d039b0efbb1,0.375,e6a6b62042f2333998b8357baa6fe8b776aaad3c0b1692df334f2afaf3954baf,WebKit;wl1,WebKit WebGL;wl2,481bdb3423f1a79606be78ed3f6cbc5d5dffd9ad02b0e61036ae14646bd96721,35,Google Inc. (NVIDIA);wl3,ANGLE (NVIDIA, NVIDIA GeForce RTX 3060 Laptop GPU (0x00002520) Direct3D11 vs_5_0 ps_5_0, D3D11);wl4,1f2ff65eb08d5fda390d5e11491bb7631888266547c82ad85f994b40b732c0bb,03b8256a6caaaef793e5e8b835cd4d2a73f0e4691691cc242286358ed93d1ff8"
+]
+PR = 7, DR = 5, hR = 10, AR = 0, SR = 1, jR = 2,
+    Qs = PR + DR * hR
+YG = 17
+qG = 1
+U5 = 23
+TV = 10
+hz = 2
+fB6 = 1000
+nz = 0
+T5 = 52
+Wl = 5
+lz = 3
+gl = 4
+WV = 36
+V5 = 38
+VV = 7
+OV = 8
+sV = 9
+v5 = 39
+cz = 13
+UC = ""
+// Vj6 = B4(57, [Cj6, 2, false])
+vC = [
+    300000,
+    4095,
+    1,
+    8,
+    20,
+    0,
+    100000,
+    10,
+    40,
+    12,
+    7,
+    15,
+    17,
+    34,
+    8888888,
+    7777777,
+    4,
+    2,
+    1000,
+    25,
+    47,
+    9,
+    0.7,
+    0.95,
+    0.4,
+    0.9,
+    0.98,
+    0.1,
+    0.07,
+    0.025,
+    255,
+    0.075,
+    0.08,
+    0.22,
+    4294967296,
+    999999,
+    5,
+    13,
+    6,
+    11,
+    18,
+    19,
+    22,
+    3600000,
+    65793,
+    4294967295,
+    4282663,
+    8388607,
+    65535,
+    32,
+    92,
+    127,
+    3,
+    57,
+    4064256,
+    3540,
+    2048,
+    90,
+    3000
+]
+
+// Vj6 = ",439,1-48,977e,"
+var Rz = function (Y8, t8) {
+    return Y8 >= t8;
+};
+var bl = function (qz6, gz6) {
+    return qz6 <= gz6;
+};
+var Pz = function (E8, W8) {
+    return E8 !== W8;
+};
+var tz = function (F8, w8) {
+    return F8 < w8;
+};
+var LC = function (z8, l8) {
+    return z8 * l8;
+};
+var mz = function (T4, U4) {
+    return T4 + U4;
+};
+var Tz = function (H8) {
+    return -H8;
+};
+var bG = function (qv6) {
+    return !qv6;
+};
+var B4 = function v4(A4, S4) {
+    'use strict';
+    var j4 = v4;
+    switch (A4) {
+        case Qs: {
+            var M4 = S4[AR];
+            var L4 = S4[SR];
+            var D4 = S4[jR];
+            // KG.push(n4); (function P4() {
+            //     KG.push(Q4);
+            //     if (Pz(typeof r4[nz], "undefined")) {
+            //         KG.pop();
+            //         return;
+            //     }
+            //     function R4(s4) {
+            //         KG.push(d4);
+            //         var X4;
+            //         return X4 = Jz(typeof s4, mz([], [][[]])) ? T6.Vv.call(null, O4, N5) : mz(mz("case ", s4), ":"),
+            //         KG.pop(),
+            //         X4;
+            //     }
+            //     var Z4 = T6["S6"].call(j4);
+            //     var k4 = Z4["indexOf"](R4(I4[nz]), N4[nz]);
+            //     var G4 = R4(z4[nz]);
+            //     var l4 = Z4["indexOf"](G4, mz(k4, V4[nz]));
+            //     var C4 = nz;
+            //     for (var F4 = k4; F4 < l4; ++F4) {
+            //         var w4 = Z4["charCodeAt"](F4);
+            //         if (w4 != TV && w4 != cz && w4 != t4) {
+            //             C4 = (C4 << Wl) - C4 + w4;
+            //             C4 = C4 | nz;
+            //         }
+            //     }
+            //     r4[nz] = C4 ? C4: qG;
+            //     T6["D6"][nz] = mz(nz, qG);
+            //     KG.pop();
+            // })();
+            if (1 > AR) {
+                // RG(r4[AR] - f4[AR]);
+            }
+            var b4;
+            var q4;
+            var g4 = bG(nz);
+            var cC = ",";
+            var TC = D4 ? hz : lz;
+            if (bG(UC) && (UC = "abcdefghijklmnopaqrstuvxyzABCDEFGHIJKLMNOPAQRSTUVXYZ!@#%&-_=;:<>,~", Rz(L4, nz) && bl(L4, sV))) for (b4 = nz; bl(b4, sV); ++b4) if (Pz(b4, L4)) for (q4 = nz; tz(q4, vC[gl]); ++q4) UC += b4["toString"]();
+            for (; ;) {
+                for (cC = ",", g4 = bG(nz), b4 = vC[Wl]; tz(b4, mz(Math["floor"](LC(Math["random"](), TC)), TC)); ++b4) {
+                    for (q4 = nz; tz(q4, mz(Math["floor"](LC(Math["random"](), TC)), TC)); ++q4) cC += UC[Math["floor"](LC(Math["random"](), UC["length"]))];
+                    cC += ",";
+                }
+                for (b4 = nz; tz(b4, M4["length"]); ++b4) if (Pz(Tz(qG), M4[b4]["toString"]()["indexOf"](cC))) {
+                    g4 = bG(vC[hz]);
+                    break;
+                }
+                if (g4) {
+                    var QC;
+                    return QC = cC,
+                        // KG.pop(),
+                        QC;
+                }
+            }
+            // KG.pop();
+        }
+            break;
+        case js: {
+            if (AR) {
+                throw Math.random();
+            }
+        }
+            break;
+    }
+};
+var hz = 2
+Vj6 = B4(Qs, [Cj6, hz, bG(bG(undefined))]) // 先不扣 搞死人的
+
+console.log(Vj6)
+
+lD6 = Cj6["join"](Vj6)
+
+vC = [
+    300000,
+    4095,
+    1,
+    8,
+    20,
+    0,
+    100000,
+    10,
+    40,
+    12,
+    7,
+    15,
+    17,
+    34,
+    8888888,
+    7777777,
+    4,
+    2,
+    1000,
+    25,
+    47,
+    9,
+    0.7,
+    0.95,
+    0.4,
+    0.9,
+    0.98,
+    0.1,
+    0.07,
+    0.025,
+    255,
+    0.075,
+    0.08,
+    0.22,
+    4294967296,
+    999999,
+    5,
+    13,
+    6,
+    11,
+    18,
+    19,
+    22,
+    3600000,
+    65793,
+    4294967295,
+    4282663,
+    8388607,
+    65535,
+    32,
+    92,
+    127,
+    3,
+    57,
+    4064256,
+    3540,
+    2048,
+    90,
+    3000
+]
+
+var mz = function (T4, U4) {
+    return T4 + U4;
+};
+
+function rS6() {
+    var H96;
+    return H96 = window["Date"]["now"] && "number" == typeof window["Date"]["now"]() ? window["Date"]["now"]() : +new window["Date"](),
+        H96;
+}
+
+var bl = function (qz6, gz6) {
+    return qz6 <= gz6;
+};
+lj6 = rS6()
+
+var Lz = function (q5, g5) {
+    return q5 > g5;
+};
+var Pz = function (E8, W8) {
+    return E8 !== W8;
+};
+var Ql = function (Y5, t5) {
+    return Y5 % t5;
+};
+var tz = function (F8, w8) {
+    return F8 < w8;
+};
+var kz = function (Wz6, fz6) {
+    return Wz6 - fz6;
+};
+var Jz = function (q8, g8) {
+    return q8 === g8;
+};
+var dz = function (XC, xC) {
+    return XC == xC;
+};
+var Rz = function (Y8, t8) {
+    return Y8 >= t8;
+};
+var j26 = function (M26, L26, D26, n26) {
+    Lz(M26, L26) && bl(M26, D26) && Lz(M26 += Ql(n26, kz(D26, L26)), D26) && (M26 = mz(kz(M26, D26), L26));
+    return M26;
+};
+var x8 = function (p8, J8) {
+    return p8 ^ J8;
+};
+
+function dL6(XR6) {
+    if (null == XR6) {
+        var xR6;
+        return xR6 = -1,
+            xR6;
+    }
+    try {
+        for (var JR6 = 0,
+                 KR6 = 0; KR6 < XR6["length"]; KR6++) {
+            var OR6 = XR6["charCodeAt"](KR6);
+            OR6 < 128 && (JR6 += OR6);
+        }
+        var ZR6;
+        return ZR6 = JR6,
+            ZR6;
+    } catch (HR6) {
+        var kR6;
+        return kR6 = -2,
+            kR6;
+    }
+}
+
+function fD6(vN6, AN6) {
+    vN6 = window["String"](vN6),
+        AN6 = window["String"](AN6);
+    var jN6 = [],
+        MN6 = AN6["length"];
+    if (Lz(MN6, 0)) {
+        for (var LN6 = 0; tz(LN6, vN6["length"]); LN6++) {
+            var DN6 = vN6["charCodeAt"](LN6),
+                nN6 = vN6["charAt"](LN6);
+            Pz(DN6 = j26(DN6, 47, vC[53], AN6["charCodeAt"](Ql(LN6, MN6))), vN6["charCodeAt"](LN6)) && (nN6 = window["String"]["fromCharCode"](DN6)),
+                jN6["push"](nN6);
+        }
+        if (Lz(jN6["length"], 0)) {
+            var PN6;
+            return PN6 = jN6["join"](""),
+                PN6;
+        }
+    }
+    var rN6;
+    return rN6 = vN6,
+        rN6;
+}
+
+WD6 = "7a74G7m23Vrp0o5c"
+bD6 = 477632
+gD6 = mz(WD6, fD6(bD6, WD6));
+console.log(gD6)
+A16 = "UEV+rrjreHyMyL863oadBw=="
+lG = 24
+
+lD6 = mz(mz(mz(mz(hz, Vj6), hz), Vj6), lD6 = mz(mz(mz(mz(mz(gD6, A16), Vj6), x8(lG, dL6(lD6))), Vj6), lD6));
+
+// console.log("第二次--》",lD6)
+
+var S16 = rS6();
+var cV = function (T46, U46) {
+    return T46 >> U46;
+};
+var bG = function (qv6) {
+    return !qv6;
+};
+
+// 获取cookie值的
+function r26(gd6) {
+    if (c6["document"]["cookie"]) for (var cX6 = "" ["concat"](gd6, "="), TX6 = c6["document"]["cookie"]["split"]("; "), UX6 = 0; UX6 < TX6["length"]; UX6++) {
+        var BX6 = TX6[UX6];
+        if (0 === BX6["indexOf"](cX6)) {
+            var vX6 = BX6["substring"](cX6["length"], BX6["length"]);
+            if (-1 !== vX6["indexOf"]("~") || -1 !== c6["decodeURIComponent"](vX6)["indexOf"]("~")) {
+                var AX6;
+                return AX6 = vX6,
+                    AX6;
+            }
+        }
+    }
+    var SX6;
+    return SX6 = !1,
+        SX6;
+}
+
+YG = 17
+TV = 10
+lz = 3
+
+function T16(bm_sz) {
+    var jX6 = [8888888, 7777777];
+    // var MX6 = r26(qd6);
+    var MX6 = bm_sz
+    if (Pz(bG(1), MX6))
+        var DX6 = window["decodeURIComponent"](MX6)["split"]("~");
+    if (Rz(DX6["length"], 4)) {
+        var nX6 = window["parseInt"](DX6[vC[YG]], TV),
+            PX6 = window["parseInt"](DX6[lz], TV);
+        jX6 = [nX6 = window["isNaN"](nX6) ? U16 : nX6, PX6 = window["isNaN"](PX6) ? B16 : PX6];
+    }
+    var rX6;
+    return rX6 = jX6,
+        rX6;
+}
+
+
+bm_sz = "CD390453CFB27896841321C34FD5464B~YAAQV/6Ycz2cPFOQAQAA7JHyWBg9DMXqPibQth/4vsYlx3xE/aRfJGzfaQkD6h6l+QTO9cneYMdCDmK5dUCa1DjxM8qbuFnYBm7D5+8HzOYKiecqSj/dpoDE+/WkF2+YVDt8+0PLFktBFLoHPfbTIFPUggm8YPv5N+WDKJa9oSkWa3sV1Vr2xmFtIXemiZycgJrSr/9a6wSuzDIv8dkLm8ZwaOoVrbSrAMjikyfd6NWV5c2bWVwEjN+YRBlmzJmYWmrUoynNXLuNn64MD0vYFflNmmvIiFFVrdqFR0MwAZdF5KO91E6A14VWlHFIflpP3IrmIAWtiU2rR1DlggRU5TGKRmkDgX/hVxKEdxBR1I5wfZBeLV/ijIXO5jlDbcrlDUxNg/mFCToP1quILw6vbt4RfYC8OgRd~4404019~4338484"
+c16 = T16(bm_sz);
+
+var Ul = function (sC, dC) {
+    return sC & dC;
+};
+
+
+kG = 48
+lV = 44
+fV = 45
+IV = 46
+mG = 47
+
+
+lD6 = function j16(M16, L16) {
+    var D16;
+    var n16;
+    var P16;
+    var Q16;
+    var r16 = M16["split"](",");
+    for (Q16 = 0; tz(Q16, r16["length"]); Q16++) D16 = Ql(Ul(cV(L16, 8), 65535), r16["length"]),
+        L16 *= vC[44],
+        L16 &= vC[45],
+        L16 += vC[46],
+        n16 = Ql(Ul(cV(L16 &= vC[47], 8), vC[kG]), r16["length"]),
+        L16 *= vC[lV],
+        L16 &= vC[fV],
+        L16 += vC[IV],
+        L16 &= vC[mG],
+        P16 = r16[D16],
+        r16[D16] = r16[n16],
+        r16[n16] = P16;
+    var h16;
+    return h16 = r16["join"](","),
+        h16;
+}(lD6, c16[1])
+
+var kz = function (Wz6, fz6) {
+    return Wz6 - fz6;
+};
+// console.log("第三次--》",lD6)
+
+S16 = kz(rS6(), S16);
+
+var R16 = rS6();
+Av6 = 127
+A5 = 49
+v5 = 39
+JV = 34
+
+ZV = 50
+x16 = ""
+t4 = 32
+OV = 8
+bV = 51
+nz = 0
+
+var Tz = function (H8) {
+    return -H8;
+};
+var J16 = new window["Array"](Av6);
+
+lD6 = function s16(d16, X16) {
+    if (bG(0)) for (var p16 = 0; tz(p16, Av6); ++p16) tz(p16, vC[A5]) || Jz(v5, p16) || Jz(JV, p16) || Jz(vC[ZV], p16) ? J16[p16] = Tz(1) : (J16[p16] = x16["length"], x16 += window["String"]["fromCharCode"](p16));
+    for (var K16 = "",
+             O16 = 0; tz(O16, d16["length"]); O16++) {
+        var Z16 = d16["charAt"](O16),
+            H16 = Ul(cV(X16, OV), vC[kG]);
+        X16 *= vC[lV],
+            X16 &= 65575,
+            X16 += vC[IV],
+            X16 &= vC[mG];
+        var k16 = J16[d16["charCodeAt"](O16)];
+        if (dz("function", typeof Z16["codePointAt"])) {
+            var m16 = Z16["codePointAt"](0);
+            Rz(m16, t4) && tz(m16, vC[bV]) && (k16 = J16[m16]);
+        }
+        Rz(k16, nz) && (k16 += Ql(H16, x16["length"]), k16 %= x16["length"], Z16 = x16[k16]),
+            K16 += Z16;
+    }
+    var I16;
+    return I16 = K16,
+        I16;
+}(lD6, c16[0])
+
+
+R16 = kz(rS6(), S16);
+
+var N16 = "" ["concat"](kz(rS6(), lj6), ",")["concat"](0, ",")["concat"](0, ",")["concat"](S16, ",")["concat"](R16, ",")["concat"](0);
+
+V16 = ';'
+lD6 = mz(mz(mz(mz(mz(mz(mz(mz(mz("2;", 0), V16), c16[nz]), V16), c16[1]), V16), N16), V16), lD6);
+
+console.log("2.0版本加密结果：", lD6)
